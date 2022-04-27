66d0c71b5555d96c94384fe689849c2ac1b6efe8
EasonC13 <gtes9950406@gmail.com>
2022-04-27 08:57:15 +0800

    fix: community experience content

--- components/CommunityExp.vue
+++ components/CommunityExp.vue
@@ -3,12 +3,22 @@
     <h2>Community Experience</h2>
     <div>
       <h3 class="h5 mb-1">
+        Google Developer Student Club (GDSC) Lead
+      </h3>
+      <ul>
+        <!-- <li>Lead (2021) and Core Team Member (2022) at NTNU</li> -->
+        <li>Hosted 3 workshops for 50+ students about Developing Python FastAPI and Chatbot</li>
+        <li>Addressed Community Talk about Google Summer of Code Experience and Tips to Apply</li>
+      </ul>
+    </div>
+    <div>
+      <h3 class="h5 mb-1">
         Conference for Open Source Coders, Users and Promoters (COSCUP)
       </h3>
       <ul>
-        <li>2021/08 - The user experience design on Natural Language Generation Chatbot</li>
-        <li>2021/08 - The changes, advantages and challenges of IOTA Chrysalis Update</li>
-        <li>2021/08 - Current status and potential future of Information Education at Taiwan</li>
+        <li>2021/08 - Talk: The user experience design on Natural Language Generation Chatbot</li>
+        <li>2021/08 - Talk: The changes, advantages and challenges of IOTA Chrysalis Update</li>
+        <li>2021/08 - Forum: Current status and potential future of Information Education at Taiwan</li>
       </ul>
     </div>
     <div>
@@ -16,18 +26,8 @@
         Students' Information Technology Conference (SITCON)
       </h3>
       <ul>
-        <li>2021/08 - How to Raise an NLP AI Chatbot?</li>
-        <li>2020/08 - Can I use Python to control my computer?</li>
-      </ul>
-    </div>
-        <div>
-      <h3 class="h5 mb-1">
-        Google Developer Student Club (GDSC) Lead
-      </h3>
-      <ul>
-        <!-- <li>Lead (2021) and Core Team Member (2022) at NTNU</li> -->
-        <li>Hosted 3 workshops for 50+ students about Developing Python FastAPI and Chatbot</li>
-        <li>Addressed Community Talk about Google Summer of Code Experience and Tips to Apply</li>
+        <li>2021/08 - Talk: How to Raise an NLP AI Chatbot?</li>
+        <li>2020/08 - Talk: Can I use Python to control my computer?</li>
       </ul>
     </div>
 	</div>
