f3fce9b9fe35f057d7f7deb03e737b81eca42d6a
EasonC13 <gtes9950406@gmail.com>
2022-05-11 20:19:50 +0800

    update: publications

--- components/Publications.vue
+++ components/Publications.vue
@@ -3,9 +3,9 @@
     <h2>Publications (Selected)</h2>
     <ul>
       <li>Wu, C.-L., Su, Y.-D., <b>Chen, E.</b>, Chen, P.-Z., Chang Y.-L., Chen, H.-C. (2022). Development and Validation of Interactive Creativity Task Platform. Frontiers in Psychology, 13, Article 771820 (SSCI).</li>
-      <li><b>Chen, E.</b>, Tseng, Y.-H., You, A., Lo, K.-P., Lin, C. (2022) Preliminary Design of an AI Service to Assist Self-regulated Learning by Edge Computing. Paper will present at International Conference on Artificial Intelligence in Education 2022 (AIED 22)</li>
+      <li><b>Chen, E.</b>, Tseng, Y.-H., You, A., Lo, K.-P., Lin, C. (2022). Preliminary Design of an AI Service to Assist Self-regulated Learning by Edge Computing. Paper will present at International Conference on Artificial Intelligence in Education (AIED'22).</li>
       <li><b>Chen, E.</b>, Tseng, Y.-H. (2022). A decision model for designing NLP applications. Paper presented at User Centered Natural Language Processing Workshop in Companion Proceedings of the Web Conference (WWW'22).</li>
-      <li><b>Chen, E.</b> (2022). The Effect of Multiple Replies for Natural Language Generation Chatbots. Paper present in Conference on Human Factors in Computing Systems Extended Abstracts (CHI EA '22). </li>
+      <li><b>Chen, E.</b> (2022). The Effect of Multiple Replies for Natural Language Generation Chatbots. Paper present at Student Research Competition in Conference on Human Factors in Computing Systems Extended Abstracts (CHI EA '22).</li>
       <li>Roche, N., Hernandez, W., <b>Chen, E.</b>, Siméon, J., & Selman, D. (2022). Ergo--a programming language for Smart Legal Contracts. Paper presented at the Programming Languages and the Law workshop at Principles of Programming Languages Conference (POPL'22).</li>
       <!-- <li><b>Chen, E.</b>, Tseng, Y.-H., & Lo, K.-P. (2021). Focus Plus: Detect Learner's Distraction by Web Camera in Distance Teaching. Paper presented at the 2021 National Chair Professorship Academic Series: Teaching and Learning in Trans - pandemic Era: Idea Innovation and Practice Transformation. </li> -->
       <li><b>Chen, E.</b> & Tseng, Y.-H. (2021). A chatbot platform that allow custom avatars and emotions and can provides multiple replies. Demo presented at the Taiwan Computer-Human Interaction Conference (TAICHI).</li>
