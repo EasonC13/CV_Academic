4ec480118ede4a936b7510597bc411fffb3ff2c7
EasonC13 <gtes9950406@gmail.com>
2022-05-11 19:18:31 +0800

    add: AIED22

--- components/Publications.vue
+++ components/Publications.vue
@@ -3,6 +3,7 @@
     <h2>Publications</h2>
     <ul>
       <li>Wu, C.-L., Su, Y.-D., <b>Chen, E.</b>, Chen, P.-Z., Chang Y.-L., Chen, H.-C. (2022). Development and Validation of Interactive Creativity Task Platform. Frontiers in Psychology, 13, Article 771820 (SSCI).</li>
+      <li><b>Chen, E.</b>, Tseng, Y.-H., You, A., Lo, K.-P., Lin, C. (2022) Preliminary Design of an AI Service to Assist Self-regulated Learning by Edge Computing. Paper will present at International Conference on Artificial Intelligence in Education 2022 (AIED 22)</li>
       <li><b>Chen, E.</b>, Tseng, Y.-H. (2022). A decision model for designing NLP applications. Paper presented at User Centered Natural Language Processing Workshop in Companion Proceedings of the Web Conference (WWW'22).</li>
       <li><b>Chen, E.</b> (2022). The Effect of Multiple Replies for Natural Language Generation Chatbots. In Conference on Human Factors in Computing Systems Extended Abstracts (CHI EA '22). </li>
       <li>Roche, N., Hernandez, W., <b>Chen, E.</b>, Siméon, J., & Selman, D. (2022). Ergo--a programming language for Smart Legal Contracts. Paper presented at the Programming Languages and the Law workshop at Principles of Programming Languages Conference (POPL'22).</li>
