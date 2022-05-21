adc273c476d2368efdbcd8e2677171c7adfb3a53
EasonC13 <gtes9950406@gmail.com>
2022-05-21 22:36:48 +0800

    update: Education

--- components/Education.vue
+++ components/Education.vue
@@ -3,10 +3,10 @@
     <h2>Education</h2>
 		<div class="row">
 			<div class="col-md">
-				<h3 class="h5">Bachelor of Information Science at National Taiwan Normal University<span class="d-md-none">,</span><br class="d-md-inline d-none">
-School of Learning Informatics, Program of Learning Sciences</h3>
+				<h3 class="h5">Bachelor of Information Science at National Taiwan Normal University (2018 - 2023)</h3>
         <ul>
-					<li>2018 - 2023 (Excepted)</li>
+					<!-- <li>2018 - 2023 (Excepted)</li> -->
+					<li>Major in Learning Sciences at School of Learning Informatics</li>
 					<li>GPA 4.12/4.3</li>
 				</ul>
 			</div>
