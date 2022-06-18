a179dd23f2c93a5a5d81dbf6649a387aa275e863
EasonC13 <gtes9950406@gmail.com>
2022-06-18 10:29:11 +0800

    update: HonorAwards

--- components/HonorAwards.vue
+++ components/HonorAwards.vue
@@ -2,11 +2,14 @@
   <div class="" id="Honors">
     <h2>Honors & Awards</h2>
 		<ul>
-      <li>Mensa Member</li>
-      <li>Dean's List at NTNU (2021)</li>
+      <li>PyCon APAC Young Inspiring (2022)</li>
+      <li>Five Virtue Awards at NTNU Graduation Ceremony (2022)</li>
       <li>Excellent Student Award and Scholarship at NTNU (2021)</li>
+      <li>Dean's List at NTNU (2021)</li>
+      <li>Mensa Member (2019 - )</li>
       <li>First Place of NTNU’s Freshman Oral Expression Competition (2019)</li>
-      <li>Collected all five of the NTNU's Five Virtue Awards: Moral, Intellectual, Physical, Social, and Aesthetic</li>
+      
+      <!-- <li>Collected all five of the NTNU's Five Virtue Awards: Moral, Intellectual, Physical, Social, and Aesthetic</li> -->
       <!-- <li>2nd Place at Hacks In Taiwan Conference (HITCON) 2021 Malware Playground Competition</li> -->
     </ul>
 	</div>
