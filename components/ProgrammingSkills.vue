5937f7ebebe1388e413c9ee9bf706dc6426dd5d3
EasonC13 <gtes9950406@gmail.com>
2022-06-24 08:08:09 +0800

    update: skills

--- components/ProgrammingSkills.vue
+++ components/ProgrammingSkills.vue
@@ -4,7 +4,7 @@
     <ul>
       <li><b>Blockchain</b>: Solidity, Hardhat, Truffle, Brownie, IOTA, Bitcoin, IPFS, Smart Legal Contract, OpenCerts</li>
       <li><b>AI & Data</b>: PyTorch, TensorFlow, Scikit-Learn, Pandas, Matplotlib, Snorkel, SHAP, GPT-3, SPSS, SmartPLS</li>
-      <li><b>Frontend</b>: Vue, Nuxt, React, Angular, TypeScript, Tensorflow.js, IndexedDB, Bootstrap, Selenium</li>
+      <li><b>Frontend</b>: Vue, React, Angular, TypeScript, Tensorflow.js, IndexedDB, Bootstrap, Tailwind CSS, Selenium</li>
       <li><b>Backend</b>: FastAPI, Flask, Django, Expresss.js</li>
       <li><b>Database</b>: MongoDB, PostgreSQL, Redis, LevelDB, Elasticsearch, Weaviate</li>
       <li><b>Cloud & Server</b>: GCP, AWS, Azure, Docker, CI/CD, Google APIs, Firebase, Ubuntu, CentOS, Nginx</li>
